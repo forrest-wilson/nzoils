9083b0a774bd6a6bd9f68c1c9d6d847a7ba30788
Forrest Wilson <forrest.wilson@me.com>
2017-10-13 21:14:30 +1300

    Working toggleable mobile navigation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-    
+    // Variable Declarations
+    var isNavOpen = false,
+        transitionTime = 300;
+
+    // Toggle mobileNav
+    $("#hamburgerToggle").click(function() {
+        // Checks to see which state the navigation is in
+        if (isNavOpen) {
+            // If the nav is open
+            closeNav();
+        } else {
+            // If the nav is closed
+            openNav();
+        }
+    });
+
+    function closeNav() {
+        $("#hamburgerToggle").removeClass("is-active-hamburger");
+        document.getElementById("mobileNav").style.width = "0";
+
+        isNavOpen = false;
+    }
+
+    function openNav() {
+        $("#hamburgerToggle").addClass("is-active-hamburger");
+        document.getElementById("mobileNav").style.width = "160px";
+
+        isNavOpen = true;
+    }
 });
