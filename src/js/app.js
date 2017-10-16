cf78f265421cbe7e9f920d7e1047628713e40e01
Forrest Wilson <forrest.wilson@me.com>
2017-10-16 20:50:58 +1300

    Genericised the scrolling events to an external function

--- src/js/app.js
+++ src/js/app.js
@@ -50,36 +50,46 @@ $(document).ready(function() {
         }, transitionTime);
     });
 
-    // Navigation item click events
-    $("#mobileNav li").click(function(e) {
-        var $href = $(this).children("a").attr("href");
+    // Generic function that requires a specific HTML structure to work
+    // $selector must be a li element with an 'a' as a child
+    function scrollNav($selector) {
+        var $href = $($selector).children("a").attr("href");
 
-        $(this).each(function() {
+        $($selector).each(function() {
             $htmlBody.animate({
                 scrollTop: $($href).offset().top - 60
             }, transitionTime);
         });
 
-        closeNav();
+        event.preventDefault();
+    }
 
-        e.preventDefault();
+    // Navigation item click events
+    $("#mobileNav li").click(function() {
+        closeNav();
+        scrollNav(this);
+    });
+
+    // Quick links click events
+    $("#contact .internal li").click(function() {
+        scrollNav(this);
     });
 
     // Shows the overlay depending on which product is clicked
-    $(".product-item").click(function(e) {
+    $(".product-item").click(function() {
         var $href = $(this).children("a").attr("href");
 
         $(this).each(function() {
             $($href).fadeIn(transitionTime);
         });
 
-        e.preventDefault();
+        event.preventDefault();
     });
 
     // Dismisses the product overlay
-    $(".fixed-close").click(function(e) {
+    $(".fixed-close").click(function() {
         $(this).parent().parent().fadeOut(transitionTime);
 
-        e.preventDefault();
+        event.preventDefault();
     });
 });
