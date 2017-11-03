26e130434c576b838217523e2cf347751a4c7b99
Forrest Wilson <forrest.wilson@me.com>
2017-11-03 20:36:57 +1300

    Working background images for slideshow

--- src/js/app.js
+++ src/js/app.js
@@ -169,7 +169,9 @@ $(document).ready(function() {
     $("#slides").slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000
+        fade: true,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true
     });
 });
